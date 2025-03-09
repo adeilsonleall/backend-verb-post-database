3be328e2f9ff743c013affcbb15c629acde0555a
Adeilson Leal <ad.eletronica.al@gmail.com>
2025-03-09 00:24:00 -0300

    update

--- validacao/validaCadastro.js
+++ validacao/validaCadastro.js
@@ -1,10 +1,22 @@
 function validaNome(nome) {
-    const validacao = isNaN(nome) && nome.length > 2;
+    const nomeUsuarioRegex = /^[a-zA-ZÀ-ÿ\s]{2,50}$/;   /*  Expressão regular:
+                                                            ^ e $: Indicam o início e o fim da string, garantindo que toda a entrada seja validada.
+                                                            [a-zA-ZÀ-ÿ\s]: Permite letras maiúsculas (A-Z), minúsculas (a-z), caracteres acentuados (À-ÿ) e espaços (\s).
+                                                            {2,50}: Exige que o nome tenha entre 2 e 50 caracteres (modifique os valores conforme necessário).
+                                                        */
+    const validacao = nomeUsuarioRegex.test(nome);
     return validacao;
 }
 
 function validaEmail(email) {
-    const validacao = isNaN(email) && email.length > 2;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;   /*   Expressão regular:
+                                                            ^[^\s@]+: Garante que o e-mail começa com um ou mais caracteres que não são espaço (\s) ou @.
+                                                            @: Exige a presença de exatamente um @ após os caracteres iniciais.
+                                                            [^\s@]+: Permite caracteres válidos no domínio (depois do @), exceto espaço ou outro @.
+                                                            \.: Exige pelo menos um ponto (.) após o domínio.
+                                                            [^\s@]+$: Garante que o e-mail termina com caracteres válidos após o ponto (sem espaços).
+                                                        */
+    const validacao = emailRegex.test(email);
     return validacao;
 }
 
@@ -17,6 +29,6 @@ export function validaUsuario(nome, email) {
     if (validacao) {
         return { status: true, mensagem: '' }
     } else {
-        return {status: false, mensagem: 'Nome ou email inválidos!'}
+        return { status: false, mensagem: 'Nome ou email inválidos!' }
     }
 }
